881b9e2700fb6f71c8de63142c6ad0a69d445a98
Syun9274 <zxczxv7624@gmail.com>
2024-01-17 22:36:17 +0900

    feat : 게시글 수정 기능

--- SpringBoot/00. codes/board/src/main/java/com/springboot/board/entity/BoardEntity.java
+++ SpringBoot/00. codes/board/src/main/java/com/springboot/board/entity/BoardEntity.java
@@ -49,4 +49,17 @@ public class BoardEntity extends BasicEntity {
         
         return boardEntity;
     }
+
+    public static BoardEntity toUpdateEntity(BoardDTO boardDTO) {
+        BoardEntity boardEntity = new BoardEntity();
+
+        boardEntity.setId(boardDTO.getId());
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(0);
+        
+        return boardEntity;
+    }
 }
