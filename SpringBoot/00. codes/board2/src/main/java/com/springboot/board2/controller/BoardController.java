44fa248068835d1faebe284578ec4c2d857caa41
Syun9274 <zxczxv7624@gmail.com>
2024-02-15 21:38:23 +0900

    feat : 게시물 검색 기능

--- SpringBoot/00. codes/board2/src/main/java/com/springboot/board2/controller/BoardController.java
+++ SpringBoot/00. codes/board2/src/main/java/com/springboot/board2/controller/BoardController.java
@@ -41,9 +41,17 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
- 
-        Page<Board> list = boardService.boardList(pageable);
+    public String boardList(Model model,
+            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+            String searchKeyword) {
+
+        Page<Board> list = null;
+
+        if (searchKeyword == null) {
+            list = boardService.boardList(pageable);
+        } else {
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
 
         int nowPage = list.getPageable().getPageNumber() + 1;
         int startPage = Math.max(nowPage - 4, 1);
@@ -61,13 +69,13 @@ public class BoardController {
     public String boardView(Model model, Integer id) {
 
         model.addAttribute("board", boardService.boardView(id));
-        
+
         return "boardview";
     }
 
     @GetMapping("/board/delete")
     public String boardDelete(Integer id) {
-        
+
         boardService.boardDelete(id);
 
         return "redirect:/board/list";
--- SpringBoot/00. codes/board2/src/main/java/com/springboot/board2/repository/BoardRepository.java
+++ SpringBoot/00. codes/board2/src/main/java/com/springboot/board2/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.springboot.board2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.springboot.board2.entity.Board;
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
     
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
